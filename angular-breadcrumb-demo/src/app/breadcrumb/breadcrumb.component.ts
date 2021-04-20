107e7b9837414a984507752f02eed6107a5a184e
Luqmaan Nakooda <>
2021-04-20 12:23:36 +0200

    Clean up.

--- angular-breadcrumb-demo/src/app/breadcrumb/breadcrumb.component.ts
+++ angular-breadcrumb-demo/src/app/breadcrumb/breadcrumb.component.ts
@@ -9,8 +9,8 @@ import { filter, distinctUntilChanged } from 'rxjs/operators';
   styleUrls: ['./breadcrumb.component.scss']
 })
 export class BreadcrumbComponent implements OnInit {
-  public breadcrumbs: IBreadCrumb[]
- 
+  public breadcrumbs: IBreadCrumb[];
+
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
@@ -20,11 +20,11 @@ export class BreadcrumbComponent implements OnInit {
 
   ngOnInit() {
     this.router.events.pipe(
-      filter((event: Event) => event instanceof NavigationEnd),
+      filter((event) => event instanceof NavigationEnd),
       distinctUntilChanged(),
     ).subscribe(() => {
       this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-    })
+    });
   }
 
   /**
@@ -34,24 +34,24 @@ export class BreadcrumbComponent implements OnInit {
    * @param breadcrumbs
    */
   buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: IBreadCrumb[] = []): IBreadCrumb[] {
-    //If no routeConfig is avalailable we are on the root path
+    // If no routeConfig is available we are on the root path
     let label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data.breadcrumb : '';
-    let isClickable = route.routeConfig && route.routeConfig.data && route.routeConfig.data.isClickable;
+    // const isClickable = route.routeConfig && route.routeConfig.data && route.routeConfig.data.isClickable;
     let path = route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
-  
+
     // If the route is dynamic route such as ':id', remove it
     const lastRoutePart = path.split('/').pop();
     const isDynamicRoute = lastRoutePart.startsWith(':');
-    if(isDynamicRoute && !!route.snapshot) {
+    if (isDynamicRoute && !!route.snapshot) {
       const paramName = lastRoutePart.split(':')[1];
       path = path.replace(lastRoutePart, route.snapshot.params[paramName]);
       label = route.snapshot.params[paramName];
     }
-  
-    //In the routeConfig the complete path is not available,
-    //so we rebuild it each time
+
+    // In the routeConfig the complete path is not available,
+    // so we rebuild it each time
     const nextUrl = path ? `${url}/${path}` : url;
-  
+
     const breadcrumb: IBreadCrumb = {
         label: label,
         url: nextUrl,
@@ -59,8 +59,8 @@ export class BreadcrumbComponent implements OnInit {
     // Only adding route with non-empty label
     const newBreadcrumbs = breadcrumb.label ? [ ...breadcrumbs, breadcrumb ] : [ ...breadcrumbs];
     if (route.firstChild) {
-        //If we are not on our current path yet,
-        //there will be more children to look after, to build our breadcumb
+        // If we are not on our current path yet,
+        // there will be more children to look after, to build our breadcrumb
         return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
     }
     return newBreadcrumbs;
